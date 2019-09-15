54352041a9acc6b7355c1291b2c418ac68422343
贾倩倩 <hannah@qianqian-mac.home>
2019-09-15 17:04:52 +1000

    update to main branch version

---
+++ TeamMeet/app/src/main/java/sydney/edu/au/teammeet/RegisterActivity.java
@@ -0,0 +1,167 @@
+package sydney.edu.au.teammeet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class RegisterActivity extends BaseActivity {
+    private static final String TAG = "Register";
+    private FirebaseAuth mAuth;
+
+    //Views
+    private EditText UserEmail, UserPassword, UserConfirmPassword,UserName;
+    private Button CreateAccountButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        UserEmail = (EditText) findViewById(R.id.Siginup_Email);
+        UserPassword = (EditText) findViewById(R.id.Signup_Password);
+        UserConfirmPassword = (EditText) findViewById(R.id.Signup_Confirmpw);
+        UserName = (EditText) findViewById(R.id.Siginup_Username);
+        CreateAccountButton = (Button) findViewById(R.id.Signup_Button);
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               CreateNewAccount();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void CreateNewAccount() {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+        String confirmPassword = UserConfirmPassword.getText().toString();
+        final String userName = UserName.getText().toString();
+        if (!validateForm()) {
+            return;
+        }
+        showProgressDialog();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(userName).build();
+                            assert user != null;
+                            user.updateProfile(profileUpdates)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()){
+                                                sendEmailVerification();
+                                                SendUserToLoginActivity();
+                                            }
+                                        }
+                                    });
+
+                            Toast.makeText(RegisterActivity.this, R.string.auth_success, Toast.LENGTH_SHORT).show();
+                        } else {
+                            String message = task.getException().getMessage();
+                            Toast.makeText(RegisterActivity.this, R.string.auth_failed + message, Toast.LENGTH_SHORT).show();
+                        }
+
+                        hideProgressDialog();
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+        String displayName = UserName.getText().toString();
+        if (TextUtils.isEmpty(displayName)) {
+            UserName.setError("User Name is required");
+            valid = false;
+        } else {
+            UserEmail.setError(null);
+        }
+
+        String email = UserEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            UserEmail.setError("Email address is required");
+            valid = false;
+        } else {
+            UserEmail.setError(null);
+        }
+
+        String password = UserPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            UserPassword.setError("Password is Required.");
+            valid = false;
+        } else {
+            UserPassword.setError(null);
+        }
+        String confirmPassword = UserConfirmPassword.getText().toString();
+        if (TextUtils.isEmpty(confirmPassword)) {
+            UserConfirmPassword.setError("Please confirm password!");
+            valid = false;
+        } else if (!password.equals(confirmPassword)) {
+            UserConfirmPassword.setError("The two passwords you entered does not match!");
+        } else {
+            UserConfirmPassword.setError(null);
+        }
+        return valid;
+    }
+
+    private void sendEmailVerification() {
+
+        // Send verification email
+        // [START send_email_verification]
+        final FirebaseUser user = mAuth.getCurrentUser();
+        assert user != null;
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this,
+                                    "Verification email sent to " + user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(RegisterActivity.this,
+                                    "Failed to send verification email.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END send_email_verification]
+    }
+}
