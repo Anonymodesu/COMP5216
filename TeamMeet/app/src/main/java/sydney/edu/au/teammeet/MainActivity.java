acaf48934c077e2f78d5dcfe72fa5005975de318
贾倩倩 <hannah@vlan-2653-10-19-179-132.student.wireless.sydney.edu.au>
2019-09-19 12:05:34 +1000

    add Michaels code

--- TeamMeet/app/src/main/java/sydney/edu/au/teammeet/MainActivity.java
+++ TeamMeet/app/src/main/java/sydney/edu/au/teammeet/MainActivity.java
@@ -9,26 +9,39 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends BaseActivity {
     //Variables for global navigation
+    private String TAG = "";
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     public Toolbar toolbar;
     private TextView pageName;
     private FirebaseAuth mAuth;
-    private String userName, userEmail;
+    private String userName, userEmail, userId;
     //Variables for main activity
     private Button TestChangePasswordBtn;
 
@@ -39,14 +52,39 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         //[START_of setup page header and navigation
         //set up page name
-        pageName=findViewById(R.id.page_name);
+        pageName = findViewById(R.id.page_name);
         pageName.setText("Home");
+
+//        FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
+//        final CollectionReference users = mFirestore.collection("Users");
+        //Query query = users.whereEqualTo("uid", mAuth.getCurrentUser().getUid());
+        /*
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (!task.isSuccessful()) {
+                    //Map<String, Object> userDetails = new HashMap<>();
+                    //userDetails.put("username", mAuth.getCurrentUser().getDisplayName());
+                    //userDetails.put("photo", null);
+                    //userDetails.put("timetable", new Timetable());
+                    //userDetails.put("groups", new ArrayList<String>());
+
+                    //users.document(mAuth.getCurrentUser().getUid()).set(userDetails);
+                }else{
+                    Log.d(TAG, "successful");
+                }
+            }
+        });
+        */
+
+
         //get current user information
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         assert user != null;
         userName = user.getDisplayName();
         userEmail = user.getEmail();
+        userId = user.getUid();
         //set up global nav drawer
         setSupportActionBar(toolbar);
         DrawerUtil.getDrawer(this, toolbar, userName, userEmail);
