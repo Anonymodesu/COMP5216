ecd1c1ccd16b5052cb77cc9e787e1f152781a528
Michael Muni <mmun6233@uni.sydney.edu.au>
2019-09-24 09:25:26 +1000

    Created Group class, rudimentary version of creating new groups working

--- TeamMeet/app/src/main/java/sydney/edu/au/teammeet/CreateNewGroupActivity.java
+++ TeamMeet/app/src/main/java/sydney/edu/au/teammeet/CreateNewGroupActivity.java
@@ -1,18 +1,59 @@
 package sydney.edu.au.teammeet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.ArrayList;
 
 public class CreateNewGroupActivity extends BaseActivity {
 
     private EditText editText;
     private Button backToGroups;
-    private Button addToDatabase; 
+    private Button addToDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_group);
+
+        editText = (EditText) findViewById(R.id.new_group);
+        backToGroups = (Button) findViewById(R.id.back_to_groups);
+        addToDatabase = (Button) findViewById(R.id.add_to_db);
+
+        backToGroups.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                setResult(RESULT_CANCELED, intent);
+                finish();
+            }
+        });
+
+        addToDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+                final CollectionReference groups = mFirestore.collection("Groups");
+                //final CollectionReference users = mFirestore.collection("Users");
+
+                ArrayList<String> coordinators = new ArrayList<String>();
+                ArrayList<String> members = new ArrayList<String>();
+                coordinators.add(mAuth.getCurrentUser().getUid());
+                Group group = new Group(coordinators, members, editText.getText().toString());
+                groups.add(group);
+
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
---
+++ TeamMeet/app/src/main/java/sydney/edu/au/teammeet/Group.java
@@ -0,0 +1,47 @@
+package sydney.edu.au.teammeet;
+
+import java.util.ArrayList;
+
+public class Group {
+    private ArrayList<String> coordinators;
+    private ArrayList<String> members;
+    private String groupName;
+
+    public Group(ArrayList<String> coordinators, ArrayList<String> members, String groupName) {
+        this.coordinators = coordinators;
+        this.members = members;
+        this.groupName = groupName;
+    }
+
+    public void setCoordinators(ArrayList<String> coordinators) {
+        this.coordinators = coordinators;
+    }
+
+    public void setMembers(ArrayList<String> members) {
+        this.members = members;
+    }
+
+    public ArrayList<String> getCoordinators() {
+        return coordinators;
+    }
+
+    public ArrayList<String> getMembers() {
+        return members;
+    }
+
+    public void addCoordinator(String coordinator) {
+        coordinators.add(coordinator);
+    }
+
+    public void addMember(String member) {
+        members.add(member);
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+}
